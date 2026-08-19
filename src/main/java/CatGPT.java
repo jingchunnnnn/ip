5d3d53ae32c959c87b55bd8faea738dd863c4a2e
jc <zhoujingchun2004@gmail.com>
2026-08-19 22:49:25 +0800

    Rename chatbot from Duke to CatGPT

---
+++ src/main/java/CatGPT.java
@@ -0,0 +1,10 @@
+public class CatGPT {
+    public static void main(String[] args) {
+        String banner = "  ____      _    ____ ____ _____ \n"
+                + " / ___|__ _| |_ / ___|  _ \\_   _|\n"
+                + "| |   / _` | __| |  _| |_) || |  \n"
+                + "| |__| (_| | |_| |_| |  __/ | |  \n"
+                + " \\____\\__,_|\\__|\\____|_|    |_|  \n";
+        System.out.println(banner);
+    }
+}
